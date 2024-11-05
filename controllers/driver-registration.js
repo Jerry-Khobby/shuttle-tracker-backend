758fe3e4ded8ed3211f1e8c3ba366d87f0474eb6
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-11-05 03:19:30 -1200

    started with the login

--- controllers/driver-registration.js
+++ controllers/driver-registration.js
@@ -7,8 +7,7 @@ const generateToken = (payload) => {
   const options = {
     expiresIn: "1h",
   };
-  const token = jwt.sign(payload, secretKey, options);
-  return token;
+  return jwt.sign(payload, secretKey, options);
 };
 
 exports.createDriver = async (req, res) => {
@@ -21,48 +20,60 @@ exports.createDriver = async (req, res) => {
     yearsOfExperience,
     licenseImage,
   } = req.body;
-  // Email regrex
+
+  // Email regex
   const emailRegrex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegrex.test(email)) {
     return res.status(400).json({ error: "Invalid email format" });
   }
-  // password validation
+
+  // Password validation
   const passwordRegrex =
     /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   if (!passwordRegrex.test(password)) {
     return res.status(400).json({
       error:
-        "Password must be at least 8 characters long and contain at least one letter, one digit and one special character",
+        "Password must be at least 8 characters long and contain at least one letter, one digit, and one special character",
     });
   }
 
   try {
     const existingUser = await Drivers.findOne({ email });
     if (existingUser) {
+      // Provide a generic error message to avoid user enumeration
       return res
-        .status(409)
-        .json({ error: "User with this email already exists" });
+        .status(400)
+        .json({ error: "User with this email already exists " });
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newDriver = new Drivers({
-      firstName: firstName,
-      surname: surname,
-      email: email,
+      firstName,
+      surname,
+      email,
       password: hashedPassword,
-      licenseImage: licenseImage,
-      licenseNumber: licenseNumber,
-      yearsOfExperience: yearsOfExperience,
+      licenseImage,
+      licenseNumber,
+      yearsOfExperience,
     });
     await newDriver.save();
+
     const token = generateToken({ _id: newDriver._id, email: newDriver.email });
-    res.status(200).json({ message: "Signup successfully", token: token });
+    res.status(200).json({ message: "Signup successful", token });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
 
-exports.driverLogin = async () => {};
+exports.driverLogin = async (req,res) => {
+  const {email,password}= req.body;
+  if(!password || !email){
+    return res.status(400).json({error:"All fields are required"});
+  }
+  const emailRegrex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+};
 
 exports.driverSignout = async () => {};
